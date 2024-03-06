cdb3f57ebd50d4d44953187089eec9beaadab6ec
Cornelio Posadas P <posadascornelio38@gmail.com>
2024-03-05 20:18:28 -0600

    make complete df

--- analyzer.py
+++ analyzer.py
@@ -12,17 +12,35 @@ class GymAnalyzer:
 
     def _read_txt(self):
         day_data = ('date', 'hour', 'exercise')
+        reps_data = ('weight', 'reps')
         day_data_list = []
+        reps_list = []
+        day_data_dict = {}
+        num_serie = 0
+        finished = False
+        last_exercise = None
         with open("gym-notes-1-mar.txt","r", encoding="utf8")as file:
             lines = file.readlines()
-            for line in lines:
-                if re.search(self.main_regex, line):
-                    try:
-                        parsed_day_exercise = list(re.search(self.main_regex, line).groups())
-                    except AttributeError:
-                        parsed_day_exercise = list(re.search(self.main_regex, line))        
-                    day_data_dict = dict(zip(day_data, parsed_day_exercise))
+            for i in range(len(lines)):
+                if re.search(self.exercise_regex, lines[i - 1]) and re.search(self.main_regex, lines[i]):
                     day_data_list.append(day_data_dict)
+                if re.search(self.main_regex, lines[i]):
+                    num_serie = 1
+                    try:
+                        parsed_day_exercise = list(re.search(self.main_regex, lines[i]).groups())
+                    except AttributeError:
+                        parsed_day_exercise = list(re.search(self.main_regex, lines[i]))
+                    day_data_dict = dict(zip(day_data, parsed_day_exercise))
+
+                elif re.search(self.exercise_regex, lines[i]):
+                    try:
+                        parsed_reps = list(re.search(self.exercise_regex, lines[i]).groups())
+                    except AttributeError:
+                        parsed_reps = list(re.search(self.exercise_regex, lines[i]))
+                    reps_data_dict = dict(zip(reps_data, parsed_reps))
+                    day_data_dict[f'Serie {num_serie}'] = reps_data_dict
+                    num_serie += 1
+                
         self._convert_date_format(day_data_list)
         return day_data_list
     
@@ -31,12 +49,17 @@ class GymAnalyzer:
             date = datetime.strptime(item["date"], "%d/%m/%Y")
             item["date"] = date.strftime('%Y-%m-%d')
     
-    def plot_calendar(self, data):
+    def _make_df(self, data):
         df = pd.DataFrame(data)
         df['Datetime'] = pd.to_datetime(df['date'], format='mixed')
-        events = pd.Series(0.1, index=df['Datetime'])
+        return df
 
-        calmap.calendarplot( events)
+    def plot_calendar(self, data):
+        df = self._make_df(data)
+        print(df)
+        events = pd.Series(0.5, index=df['Datetime'])
+        print(events)
+        calmap.calendarplot(events)
         plt.show()
 
 
